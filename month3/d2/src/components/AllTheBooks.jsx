e36e9cb979597f27ae7c42d0f3343cd89b50f926
MarcoDeLuca2405 <Marco_De_luca2405@hotmail.com>
2023-03-29 18:49:27 +0200

    .

--- month3/d2/src/components/AllTheBooks.jsx
+++ month3/d2/src/components/AllTheBooks.jsx
@@ -2,21 +2,55 @@ import { Component } from "react";
 import Card from 'react-bootstrap/Card';
 import Col from "react-bootstrap/esm/Col";
 import Row from "react-bootstrap/esm/Row";
-import Books from '../books/fantasy.json';
-import React, { useState } from "react";
-
+import Fantasy from '../books/fantasy.json';
+import History from '../books/history.json';
+import Horror from '../books/horror.json';
+import Romance from '../books/romance.json';
+import Scifi from '../books/scifi.json';
+import Dropdown from "react-bootstrap/Dropdown";
 class AllTheBooks extends Component {
      
+     ArrayBooks= [Fantasy,History,Horror,Romance,Scifi];
+
+    state= {
+        selectGender:null,
+    }
 
 
 render(){
  
     return(
         <div className="container-fluid m-0 my-3">
+
+
+
+    <Dropdown>
+      <Dropdown.Toggle variant="success" id="dropdown-basic">
+        Seleziona Genere
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu>
+
+        {this.ArrayBooks.map((Gender,index)=>{
+            console.log(Gender[index].category)
+            return( 
+                
+            <Dropdown.Item onClick={()=>{
+                this.setState({selectGender:Gender})
+            }} >{Gender[index].category}</Dropdown.Item> 
+            )
+        })}
+        
+       
+      </Dropdown.Menu>
+    </Dropdown>
+
+
+
         <Row className="justify-content-center ">
            
          
-                {Books.map((book)=>{
+              {this.state.selectGender?.map((book)=>{
 
                     return(
                         <Col className="Mybook my-3" xs={6} xm={4} xl={3}>
@@ -42,7 +76,7 @@ render(){
                 })}
             
             
-        </Row>
+            </Row>  
         </div>
     );
     
