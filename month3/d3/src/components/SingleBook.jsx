65a24171eef0678198e4535311c39974d8c19e0f
MarcoDeLuca2405 <Marco_De_luca2405@hotmail.com>
2023-03-30 18:21:03 +0200

    .

--- month3/d3/src/components/SingleBook.jsx
+++ month3/d3/src/components/SingleBook.jsx
@@ -1,3 +1,4 @@
+import { Component } from 'react';
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/esm/Col';
 import Container from 'react-bootstrap/esm/Container';
@@ -5,25 +6,44 @@ import Row from 'react-bootstrap/Row';
 
 
 
-const SingleBook =(props) =>
+class SingleBook extends Component {
+
+
+state={
+
+    selected:false
+
+}
+
+render(){
+
+return(
 
 <Container className='d-flex justify-content-center'>
     <Row className='mt-4'>
         <Col className='justify-content-center'>
-<Card key={props.key} style={{ width: '18rem' }}>
-<Card.Img variant="top" src={props.libri.img} />
+<Card key={this.props.key} style={{ width: '18rem' }} onClick={()=>{this.setState({ selected:true})}} className={this.state.selected&&("bg-danger")}>
+
+<Card.Img variant="top" src={this.props.libri.img} />
 <Card.Body>
-  <Card.Title>{props.libri.title}</Card.Title>
+  <Card.Title>{this.props.libri.title}</Card.Title>
   <Card.Text>
-    {props.libri.category}
+    {this.props.libri.category}
   </Card.Text>
   <Card.Footer>
-    {props.libri.price}$
+    {this.props.libri.price}$
   </Card.Footer>
 </Card.Body>
 </Card>
 </Col>
 </Row>
 </Container>
+
+
+)
+
+}
+
+}
 
 export default SingleBook;
